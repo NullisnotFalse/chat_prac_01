5fe0a626cc1e47babe5e726380ece8ea2c34c178
louis N <louis_n@naver.com>
2023-06-08 18:28:24 +0900

    [추가] 중간 저장
    - django channels 튜토리얼 진행 중
    - docker 세팅
    - redis 세팅

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from chat import views
 
 urlpatterns = [
-    path("", views.ChatView.as_view(), name="chat_view"),
+    path("", views.index, name="index"),
+    path("<str:room_name>/", views.room, name="room"),
 ]
--- chat_prac/wsgi.py
+++ chat_prac/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for chat_prac project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# mysite/asgi.py
 import os
 
-from django.core.wsgi import get_wsgi_application
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_prac.settings")
 
-application = get_wsgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
