5d4975ce660058921dd009a79e2bd46af09e4c1f
louis N <louis_n@naver.com>
2023-06-27 16:37:32 +0900

    [수정] 수정수정

--- chat_prac/asgi.py
+++ chat_prac/asgi.py
@@ -1,24 +1,20 @@
-import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import os
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_prac.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
 
 
 import chat.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
         ),
--- chat_prac/settings.py
+++ chat_prac/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from decouple import config
 import my_settings
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
